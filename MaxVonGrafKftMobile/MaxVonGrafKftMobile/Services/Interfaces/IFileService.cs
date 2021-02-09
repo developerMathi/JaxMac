9f54fec680a8eb0addd1c7bc658316f061252eac
developerMathi <54667723+developerMathi@users.noreply.github.com>
2021-02-09 16:50:50 +0530

    adg

---
+++ MaxVonGrafKftMobile/MaxVonGrafKftMobile/Services/Interfaces/IFileService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MaxVonGrafKftMobile.Services.Interfaces
+{
+    public interface IFileService
+    {
+        String GetStorageFolderPath();
+    }
+}
