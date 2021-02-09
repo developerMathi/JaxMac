f9e141e15d6bb3f2603fc16375c8b3939c4020d2
developerMathi <54667723+developerMathi@users.noreply.github.com>
2021-02-09 17:44:30 +0530

    images

---
+++ MaxVonGrafKftMobile/MaxVonGrafKftMobile.iOS/ExtendedListViewRenderer.cs
@@ -0,0 +1,33 @@
+﻿using Foundation;
+using MaxVonGrafKftMobile.iOS;
+using MaxVonGrafKftMobile.Renders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(ExtendedListView), typeof(ExtendedListViewRenderer))]
+
+namespace MaxVonGrafKftMobile.iOS
+{
+    public class ExtendedListViewRenderer : ListViewRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<ListView> e)
+        {
+            base.OnElementChanged(e);
+
+            if (e.NewElement != null)
+            {
+                if (Control != null)
+                {
+                    Control.AllowsSelection = false;
+                    Control.AlwaysBounceVertical = false;
+                    Control.Bounces = false;
+                }
+            }
+        }
+    }
+}
